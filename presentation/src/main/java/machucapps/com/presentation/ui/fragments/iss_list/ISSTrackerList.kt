16e1e4082aa0388551235417a557628dd47a990f
Fernando Sanz <machucapps@gmail.com>
2022-04-23 07:45:14 +0200

    Requested location permission

--- presentation/src/main/java/machucapps/com/presentation/ui/fragments/iss_list/ISSTrackerList.kt
+++ presentation/src/main/java/machucapps/com/presentation/ui/fragments/iss_list/ISSTrackerList.kt
@@ -1,14 +1,19 @@
 package machucapps.com.presentation.ui.fragments.iss_list
 
+import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.dialogs.SettingsDialog
+import machucapps.com.isstracker.R
 import machucapps.com.isstracker.databinding.IssTrackerListFragmentBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class ISSTrackerList : Fragment() {
+class ISSTrackerList : Fragment(), EasyPermissions.PermissionCallbacks {
 
     private val viewModel by viewModel<ISSTrackerListViewModel>()
 
@@ -26,10 +31,49 @@ class ISSTrackerList : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requestLocationPermission()
+    }
+
+    private fun requestLocationPermission() {
+        EasyPermissions.requestPermissions(
+            this,
+            getString(R.string.location_permissions_message),
+            PERMISSION_LOCATION_REQUEST_CODE,
+            PERMISSION
+        )
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        Log.d(TAG, "Permission not granted")
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            SettingsDialog.Builder(requireActivity()).build().show()
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
+        Log.d(TAG, "Permission granted")
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        // The EasyPermissions library is not up to date and still needs this deprecated method.
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    companion object {
+        private const val PERMISSION_LOCATION_REQUEST_CODE = 1
+        private const val PERMISSION = Manifest.permission.ACCESS_FINE_LOCATION
+        private const val TAG = "ISSTrackerList"
     }
 }
